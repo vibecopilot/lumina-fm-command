1517066daa3bdfd6796abbf26501ba04bd5d1602
akshaymugale01 <akshaymugale01@gmail.com>
2026-03-02 06:32:58 +0530

    lastest

--- src/apis/axiosinstance.ts
+++ src/apis/axiosinstance.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000',
+  // baseURL: 'http://localhost:3000',
+  baseURL: 'https://admin.vibecopilot.ai',
+
   headers: {
     'Content-Type': 'application/json',
   },
