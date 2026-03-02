50e3909a8ab0101d2d93d16f889414c9b33834df
akshaymugale01 <akshaymugale01@gmail.com>
2026-03-02 06:26:52 +0530

    user

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import UrlParamSync from "./components/UrlParamSync";
+import UrlParamSync from "./pages/GroupedDashboardPage";
 
 const queryClient = new QueryClient();
 
